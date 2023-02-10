b9db5f1a07e7c7e3277fb247225a70f3cf132e92
bdoey <115423679+bdoey@users.noreply.github.com>
2023-02-09 21:17:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,12 @@ import streamlit as st
 
 st.markdown("# This fuckin guy...")
 
-# Display an image from a URL
-image_url = "https://github.com/bdoey/FUjkal/blob/master/SCR-20221215-spp-2v1.jpg?raw=true"
-st.image(image_url, width=700)
-
 if st.button('Do you like this guy?'):
     st.write("We don't either!")
 # else:
 #     st.write('Goodbye')
+
+# Display an image from a URL
+image_url = "https://github.com/bdoey/FUjkal/blob/master/SCR-20221215-spp-2v1.jpg?raw=true"
+st.image(image_url, width=700)
+
