2f58e93ae53841941a5d20900e2df4442f02158c
bdoey <115423679+bdoey@users.noreply.github.com>
2023-02-09 21:12:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.markdown("# This guy...")
 
 # Display an image from a URL
 image_url = "https://github.com/bdoey/FUjkal/blob/master/SCR-20221215-spp-2v1.jpg?raw=true"
-st.image(image_url, width=300)
+st.image(image_url, width=700)
