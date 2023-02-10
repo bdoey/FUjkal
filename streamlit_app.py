41a34c9499196669d00f5ee7af4a429dcbed376e
bdoey <115423679+bdoey@users.noreply.github.com>
2023-02-09 21:30:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,8 @@ st.markdown("\n Do you like this guy?")
 
 if st.button('Nope!'):
     st.write("We don't either!")
-elif:
-    st.button('Yup!')
-    st.write('Goodbye')
+else st.button('Yup!'):
+    st.write('Goodbye loser.')
 
 # Display an image from a URL
 image_url = "https://github.com/bdoey/FUjkal/blob/master/SCR-20221215-spp-2v1.jpg?raw=true"
