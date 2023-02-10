671ce5c24e96d59904253c88353fea2ceff96300
bdoey <115423679+bdoey@users.noreply.github.com>
2023-02-09 21:26:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ if st.button('Do you like this guy?'):
 
 # Display an image from a URL
 image_url = "https://github.com/bdoey/FUjkal/blob/master/SCR-20221215-spp-2v1.jpg?raw=true"
-st.image(image_url, width=700)
+st.image(image_url, width=300)
 
